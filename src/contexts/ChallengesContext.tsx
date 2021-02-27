50bcb4c6f86046c8a870fa2100c5cd7adf8cf21e
João Luiz <jleaopitta@gmail.com>
2021-02-27 13:29:01 -0300

    Corrigida barra de experiência que já aparecia preenchida**

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -34,9 +34,9 @@ export const ChallengesProvider: React.FC<ChallengeProviderProps> = ({
   children,
   ...rest
  }) => {
-  const [level, setLevel] = useState(rest.level ?? 1);
-  const [currentExperience, setCurrentExperience] = useState(rest.currentExperience ?? 0);
-  const [challengesCompleted, setChallengesCompleted] = useState(rest.challengesCompleted ?? 0);
+  const [level, setLevel] = useState(rest.level);
+  const [currentExperience, setCurrentExperience] = useState(rest.currentExperience);
+  const [challengesCompleted, setChallengesCompleted] = useState(rest.challengesCompleted);
   const [isLevelUpModalOpen, setIsLevelUpModalOpen] = useState(false);
   const [activeChallenge, setActiveChallenge] = useState(null);
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -53,9 +53,9 @@ export const getServerSideProps: GetServerSideProps = async ( context ) => {
   const {level, currentExperience, challengesCompleted} = context.req.cookies;
   return {
     props: {
-      level: Number(level),
-      currentExperience: Number(currentExperience),
-      challengesCompleted: Number(challengesCompleted)
+      level: Number(level ?? 1),
+      currentExperience: Number(currentExperience ?? 0),
+      challengesCompleted: Number(challengesCompleted ?? 0)
     }
   }
 }
