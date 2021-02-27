3ad102080da77dc1c499f2cd313ac184d8447b3c
João Luiz <jleaopitta@gmail.com>
2021-02-27 12:42:42 -0300

    Corrigida barra de experiência que já começava preenchida

--- src/components/ExperienceBar/index.tsx
+++ src/components/ExperienceBar/index.tsx
@@ -5,7 +5,7 @@ import styles from '@/styles/components/ExperienceBar.module.css';
 const ExperienceBar = () => {
   const { currentExperience, experienceToNextLevel } = useContext(ChallengesContext);
 
-  const percentToNextLevel = Math.round(currentExperience * 100) / experienceToNextLevel;
+  const percentToNextLevel = Number(Math.round(currentExperience * 100) / experienceToNextLevel);
 
   return (
     <header className={styles.experienceBar}>
